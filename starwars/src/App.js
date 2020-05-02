3a862ecf661c0aa900e76bfdad5c078017dd8e6a
Aliadabi <Aliadabi3780@gmail.com>
2020-05-02 10:01:17 -0700

    Mvp done

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios'
+import {useState , useEffect} from 'react'
+import CharacterList from './components/CharacterList';
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +15,21 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+    const MainText = styled.h1`
+    font-family: 'Reenie Beanie', cursive;
+    color:  white;
+    font-weight: bold;
+    font-size: 60px;
+    text-shadow: 2px 2px black;
+    padding: 5px;
+    margin: 0;
+    `;
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <MainText className="Header">Characters</MainText>
+      <CharacterList />
+
     </div>
   );
 }
